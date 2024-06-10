7726680f7f168e523d85d0be06cd7fd022a9ecb2
Arnav495 <arnieincyberland@gmail.com>
2024-06-09 19:17:26 -0700

    Moved some stuff to a new file

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,11 @@
 #include <curses.h>
 #include <signal.h>
 #include <thread>
-#include <unordered_map>
 #include <vector>
 #include <limits>
 #include <random>
+
+#include "sextantBlocks.cpp"
 
 using namespace std;
 
@@ -25,100 +26,6 @@ using namespace std;
 #define BIRD_HEIGHT 2
 #define BIRD_WIDTH 3
 const array<char[BIRD_WIDTH*4 + 1], BIRD_HEIGHT> BIRD_TEXT = {"🬭🬞█", "🬊█🬄"};
-
-char packBoolArray(array<array<bool, 3>, 2>& boolArray) {
-	return (
-		(boolArray[0][0] >> 0) +
-		(boolArray[0][1] >> 1) +
-		(boolArray[0][2] >> 2) +
-		(boolArray[1][0] >> 3) +
-		(boolArray[1][1] >> 4) +
-		(boolArray[1][2] >> 5)
-	);
-}
-
-// Numbered:
-// 0 3
-// 1 4
-// 2 5
-
-unordered_map<char, wchar_t> sextantMap {
-	{0b000000, L'⠀'},
-	{0b000001, L'🬞'},
-	{0b000010, L'🬇'},
-	{0b000011, L'🬦'},
-	{0b000100, L'🬁'},
-	{0b000101, L'🬠'},
-	{0b000110, L'🬉'},
-	{0b000111, L'▐'},
-	{0b001000, L'🬏'},
-	{0b001001, L'🬭'},
-	{0b001010, L'🬖'},
-	{0b001011, L'🬵'},
-	{0b001100, L'🬑'},
-	{0b001101, L'🬯'},
-	{0b001110, L'🬘'},
-	{0b001111, L'🬷'},
-	{0b010000, L'🬃'},
-	{0b010001, L'🬢'},
-	{0b010010, L'🬋'},
-	{0b010011, L'🬩'},
-	{0b010100, L'🬅'},
-	{0b010101, L'🬤'},
-	{0b010110, L'🬍'},
-	{0b010111, L'🬫'},
-	{0b011000, L'🬓'},
-	{0b011001, L'🬱'},
-	{0b011010, L'🬚'},
-	{0b011011, L'🬹'},
-	{0b011100, L'🬔'},
-	{0b011101, L'🬳'},
-	{0b011110, L'🬜'},
-	{0b011111, L'🬻'},
-	{0b100000, L'🬀'},
-	{0b100001, L'🬟'},
-	{0b100010, L'🬈'},
-	{0b100011, L'🬧'},
-	{0b100100, L'🬂'},
-	{0b100101, L'🬡'},
-	{0b100110, L'🬊'},
-	{0b100111, L'🬨'},
-	{0b101000, L'🬐'},
-	{0b101001, L'🬮'},
-	{0b101010, L'🬗'},
-	{0b101011, L'🬶'},
-	{0b101100, L'🬒'},
-	{0b101101, L'🬰'},
-	{0b101110, L'🬙'},
-	{0b101111, L'🬸'},
-	{0b110000, L'🬄'},
-	{0b110001, L'🬣'},
-	{0b110010, L'🬌'},
-	{0b110011, L'🬪'},
-	{0b110100, L'🬆'},
-	{0b110101, L'🬥'},
-	{0b110110, L'🬎'},
-	{0b110111, L'🬬'},
-	{0b111000, L'▌'},
-	{0b111001, L'🬲'},
-	{0b111010, L'🬛'},
-	{0b111011, L'🬺'},
-	{0b111100, L'🬕'},
-	{0b111101, L'🬴'},
-	{0b111110, L'🬝'},
-	{0b111111, L'█'}
-};
-
-class SextantDrawing {
-	private:
-		vector<vector<bool>> drawing;
-	
-	public:
-		SextantDrawing(const vector<vector<bool>>& setDrawing) {
-			this->drawing = setDrawing;
-		}
-		void render(double topLeftX, double topLeftY) const
-};
 
 struct Pipe {double xPos; int height;};
 struct Bird {double yPos; double yVel;};
